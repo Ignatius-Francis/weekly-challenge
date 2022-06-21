d5dd0cc8566a34467ee690a283a1bb63a20fca20
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2022-06-21 12:44:22 +0100

    First commit

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -5,9 +5,9 @@ function filterArray(arr) {
 }
 filterArray([1, 2, "a", "b"]);
     filterArray([4, "z", "f", 5]);
-    filterArray(["$%^", 567, "&&&"])
-    filterArray(["w", "r", "u", 43, "s", "a", 76, "d", 88]);
+    filterArray(["$%^", 567, "&&&"]);
+    filterArray(["w", "r", "u", 43, "s", "a", 76, "d", 88]); 
     filterArray([16, "op", 20, "[[", 24, 26, "yy"]);
     filterArray([1, "a", "b", 0, 15]);
-   filterArray([1, 2, "aasf", "1", "123", 123]); 
+   filterArray([1, 2, "aasf", "1", "123", 123]);
 module.exports = filterArray
